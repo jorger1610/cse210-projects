453f5d2a7a4976af5a6bbe18553f14359cfca945
unknown <rom19022@byui.edu>
2025-01-15 10:31:12 -0700

    Prep 3 Completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        //Random randomGenerator = new Random();
+        
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+
+        int x = int.Parse(magicNumber);
+        int y = int.Parse(userGuess); 
+
+        if (y > x)
+        {
+            Console.WriteLine("Lower ");
+        } 
+         else if (y < x)
+        {
+            Console.WriteLine("Higher ");
+        }
+
+        while (y != x)
+        {
+            Console.Write("What is your guess? ");
+            string newGuess = Console.ReadLine();
+            y = int.Parse(newGuess);
+
+            {
+                if (y == x)
+                {
+                    Console.WriteLine("You guessed it! ");
+                }
+
+                else if (y > x)
+                {
+                    Console.WriteLine("Lower ");
+                }
+
+                else if (y < x)
+                {
+                    Console.WriteLine("Higher ");
+                }
+            }
+
+        }
+    }   
 }
