4bc8b90407590f710cff2b37256d08e9f3c8d525
unknown <rom19022@byui.edu>
2025-01-19 10:50:23 +0100

    Prep 4 Completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> number = new List<int>();
+
+        number.Add(18);
+        number.Add(36);
+        number.Add(2);
+        number.Add(48);
+        number.Add(6);
+        number.Add(12);
+        number.Add(9);
+        number.Add(0);
+
+        int sum = number.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+        
+        double average = Queryable.Average(number.AsQueryable());
+        Console.WriteLine($"The average is: {average}");
+
+        int biggest = number.Max();
+        Console.WriteLine($"The largest number is: {biggest}");
+        
     }
 }
